b65c5722339f8ca5c539f614f3211cf426af977d
ivonnatalia1107 <ivon.natalia.1107@gmail.com>
2023-11-26 21:16:08 -0400

    mercadopago

--- chekckout/Checkout.js
+++ chekckout/Checkout.js
@@ -1,18 +1,11 @@
 import '../models/Product.model';
-const express = require('express');
-const app = express();
 
-// MERCADO PAGO
-app.use(express.json());
 const mercadopago = require("mercadopago")
 const { update } = require('../models/Product.model')
 
 mercadopago.configure({
     access_token: "TEST-2038802161039729-112611-1d33ccf9c7b0ed62ef505dcc2ceac319-1567073288"
 })
-
-
-// C. CHECKOUT MERCADOPAGO
 
 
 app.post("/mercadopago", async (req, res) => {
