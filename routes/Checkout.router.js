2cb197be4b5c905d9bff24f78286d781ac7f9a0b
ivonnatalia1107 <ivon.natalia.1107@gmail.com>
2023-11-28 23:10:39 -0400

    auth para comprar

--- routes/Checkout.router.js
+++ routes/Checkout.router.js
@@ -3,12 +3,13 @@ const mercadopago = require("mercadopago");
 const router = express.Router();
 const accessTokenMP = "TEST-1911582956099988-112611-4e90dcedd06c521a812c2f5210705a2c-383911558"
 const {checkoutProduct} = require('../controllers/Checkout.controller')
+const auth = require('../middlewares/auth')
 
 mercadopago.configure({
     access_token: accessTokenMP || "",
 })
 
-router.post("/", checkoutProduct)
+router.post("/",auth, checkoutProduct)
 
 
 module.exports = router
